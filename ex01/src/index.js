55bca41b428df59b9596da0dcc4a6883fd2b6e32
zabec <denis.imsirovic@gmail.com>
2022-04-13 08:59:19 +0200

    ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+
+const array1 = ['MON', 'TUE', 'WED', 'THU', 'FRI'];
+
+function main(arr) {
+  let array2 = [...array1];
+  return array2;
+}
+
+console.log(main(array1));
+module.exports = main;
