de74334bf94b3581607ecbc54cc0c9bde5d97570
zabec <denis.imsirovic@gmail.com>
2022-04-14 07:23:10 +0200

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+const BAKING_TEMPERATURES = {
+  cake: { low: 180, high: 250 },
+  bread: { low: 150, high: 250 },
+  pie: { low: 200, high: 230 }
+}
+
+function main(baking) {
+  const { bread: { low: lowBread, high: highBread } } = BAKING_TEMPERATURES;
+  return { lowBread, highBread };
+}
+
+console.log(main(BAKING_TEMPERATURES));
+module.exports = main;
